e30be8bfb1354ceca157e8c376bba7ea11071218
JiYongdong <jyd119@qq.com>
2015-02-05 12:08:00 +0800

    sac example: first version

---
+++ examples/sac/script/notice.js
@@ -0,0 +1,35 @@
+(function($) {
+  $(function() {
+    $('.notice').each(function() {
+      var notice = this;
+
+      $('.notice-head a', this).click(function() {
+        $(this).parent().children().removeClass('selected');
+        $(this).addClass('selected');
+        var i = $('.notice-head a', notice).index(this);
+        $('.notice-list', notice).hide()
+          .eq(i).show();
+        return false;
+      }).eq(0).click();
+
+      $('.notice-list', this).each(function() {
+        var nl = this;
+
+        $('ul:gt(0)', nl).hide();
+
+        $('.left-menu a', nl).click(function() {
+          $(nl).find('ul:visible').hide();
+          
+          var i = $('.left-menu a', nl).index(this);
+          $('ul', nl).eq(i).show();
+
+          $('.left-menu a', nl).removeClass('cur');
+          $(this).addClass('cur');
+          return false;
+        }).eq(0).click();
+      });
+
+    });
+
+  });
+})($);
